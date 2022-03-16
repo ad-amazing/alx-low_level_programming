9941e7d8b560e17c8764ba92ba99ea14c82ab93f
Adaeze Nwaturuocha <nwaturuochaadaeze@gmail.com>
2022-03-15 19:24:32 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+lute value of an integer
+ * @n: the int to check
+ #include "main.h"
+
+/**
+ * _abs - computes the abso* Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
