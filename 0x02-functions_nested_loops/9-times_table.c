1b4b60b5f15fa1579ac05374167bb8796e48bac3
Adaeze Nwaturuocha <nwaturuochaadaeze@gmail.com>
2022-03-17 02:11:32 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,21 +3,21 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-void times_tab)
+void times_table(void)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			k = j * i;
-le(void)
-{
-	int i, j, k;
+		int i, j, k;
 
-	for (i = 0; i < 10; i++			if (j == 0)
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				k = j * i;
+				
+				if (j == 0)
 			{
 				_putchar(k + '0');
 			}
-
-			if (k < 10 && j != 0)
+				if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
