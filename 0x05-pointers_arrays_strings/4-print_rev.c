d9838752035778cf4b2aa672fd95f868e7a095dc
Adaeze Nwaturuocha <nwaturuochaadaeze@gmail.com>
2022-03-21 21:09:12 -0700

    Print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,3 +11,16 @@ for (c = len - 1; c >= 0; c--)
 putchar(s[c]);
 putchar('\n');
 }
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
