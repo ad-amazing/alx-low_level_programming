5db766e58892da5473899aa86b91d9660246a3a5
Adaeze Nwaturuocha <nwaturuochaadaeze@gmail.com>
2022-03-21 20:02:55 -0700

    Strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- *  * _strlen- display length of a string
- *   * @s : string to mesure
- *    *
- *     * Return: len
- *      */
+ * _strlen- display length of a string
+ * @s : string to mesure
+ * Return: len
+ */
 int _strlen(char *s)
 {
 	int len = 0;
-	while (*s++)
+	while (*s++) 
 		len++;
 	return (len);
 }
