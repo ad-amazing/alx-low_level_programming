3e6f4e739b33a9a9b7cefde10d248a14d45fcf0a
Adaeze Nwaturuocha <nwaturuochaadaeze@gmail.com>
2022-03-24 22:08:02 -0700

    Print Numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,8 +4,9 @@
  */
 void print_numbers(void)
 {
-	int num;
-	for (num = 0; num <= 9; num++)
-	_putchar((num % 10) + '0');
-	_putchar('\n');
+int num;
+
+for (num = 0; num <= 9; num++)
+_putchar((num % 10) + '0');
+_putchar('\n');
 }
