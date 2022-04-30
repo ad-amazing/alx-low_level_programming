8e939c1841341e00883c807cdd8158710dfd053a
Adaeze Nwaturuocha <nwaturuochaadaeze@gmail.com>
2022-04-29 19:08:21 -0700

    Flip bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * flip_bits - ...
+  * @n: ...
+  * @m: ...
+  *
+  * Returnt n, unsigned long int m)
+{
+	unsigned long int man: ...
+  */
+unsigned int flip_bits(unsigned long isk = 0, count = 0;
+
+	mask = n ^ m;
+	while (mask)
+	{
+		if (mask & 1)
+			count++;
+
+		mask >>= 1;
+	}
+
+	return (count);
+}
