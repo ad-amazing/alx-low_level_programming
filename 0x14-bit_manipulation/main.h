76e37a02517728b97d997423d73088356f8142c6
Adaeze Nwaturuocha <nwaturuochaadaeze@gmail.com>
2022-04-29 18:47:53 -0700

    Print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,5 +4,7 @@
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
+void print_binary(unsigned long int n);
+void _divide(unsigned long int n);
 
 #endif
